fb7946cb4c6605b4d47a9b0187c5dbd6891adeca
Luciano Brd <lucien.burdet@gmail.com>
2018-06-13 17:31:45 +0200

    Ajout logo et video

--- inscription.php
+++ inscription.php
@@ -64,22 +64,22 @@
   Favicons
   =============================================
   -->
-  <link rel="apple-touch-icon" sizes="57x57" href="assets/images/favicons/apple-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="assets/images/favicons/apple-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="assets/images/favicons/apple-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="assets/images/favicons/apple-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="assets/images/favicons/apple-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="assets/images/favicons/apple-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="assets/images/favicons/apple-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="assets/images/favicons/apple-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicons/apple-icon-180x180.png">
-  <link rel="icon" type="image/png" sizes="192x192" href="assets/images/favicons/android-icon-192x192.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicons/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="assets/images/favicons/favicon-96x96.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicons/favicon-16x16.png">
+  <link rel="apple-touch-icon" sizes="57x57" href="assets/images/favicons/logo.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="assets/images/favicons/logo.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="assets/images/favicons/logo.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/images/favicons/logo.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="assets/images/favicons/logo.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="assets/images/favicons/logo.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="assets/images/favicons/logo.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="assets/images/favicons/logo.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicons/logo.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="assets/images/favicons/logo.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicons/logo.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="assets/images/favicons/logo.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicons/logo.png">
   <link rel="manifest" href="/manifest.json">
   <meta name="msapplication-TileColor" content="#ffffff">
-  <meta name="msapplication-TileImage" content="assets/images/favicons/ms-icon-144x144.png">
+  <meta name="msapplication-TileImage" content="assets/images/favicons/logo.png">
   <meta name="theme-color" content="#ffffff">
   <!--
   Stylesheets
@@ -134,14 +134,24 @@
         </div>
       </nav>
       <div class="main">
-        <section class="module bg-dark-30" data-background="assets/images/section-1.jpg">
+        <!--<section class="module bg-dark-30" data-background="assets/images/section-1.jpg">
           <div class="container">
             <div class="row">
               <div class="col-sm-6 col-sm-offset-3">
                 <h1 class="module-title font-alt mb-0">S'inscrire</h1>
               </div>
             </div>
+          </div> -->
+        </section>
+        <section class="module module-video bg-dark-30" data-background="assets/images/section-1.jpg">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-6 col-sm-offset-3">
+                <h2 class="module-title font-alt mb-0"></h2>
+              </div>
+            </div>
           </div>
+          <div class="video-player" data-property="{videoURL:'https://www.youtube.com/watch?v=C2HkDnsDV7U', containment:'.module-video', startAt:0, mute:true, autoPlay:true, loop:true, opacity:1, showControls:false, showYTLogo:false, vol:25}"></div>
         </section>
         <section class="module">
           <div class="container">
