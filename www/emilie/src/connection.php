c4cadd207271da2434c7faadb9ed0ceb81b0c358
Luciano Brd <lucien.burdet@gmail.com>
2018-08-05 18:49:47 +0200

    Dell dossier de dev

--- www/emilie/src/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-  try {
-    $db = new PDO('mysql:host=emiliengzs321.mysql.db;dbname=emiliengzs321;charset=utf8', 'emiliengzs321', 'Capiitainecool01');
-  } catch(Exception $e){
-    die('Erreur : '.$e->getMessage());
-  }
-?>
