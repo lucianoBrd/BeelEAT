67a48e8c655d923eb0855febc0068199348141ab
Luciano Brd <lucien.burdet@gmail.com>
2018-08-05 14:52:50 +0200

    Modif bug

--- src/connection.php
+++ src/connection.php
@@ -1,6 +1,6 @@
 <?php
   try {
-    $db = new PDO('mysql:host=localhost;dbname=formation_members;charset=utf8', 'root', '');
+    $db = new PDO('mysql:host=emiliengzs321.mysql.db;dbname=emiliengzs321;charset=utf8', 'emiliengzs321', 'Capiitainecool01');
   } catch(Exception $e){
     die('Erreur : '.$e->getMessage());
   }
