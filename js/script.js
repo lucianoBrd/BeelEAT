6028594e59ae13bb8fcba224d6de811b65921049
Luciano Brd <lucien.burdet@gmail.com>
2018-09-01 11:21:01 +0200

    Verification email js

--- js/script.js
+++ js/script.js
@@ -3,8 +3,29 @@ window.onload = function () {
     var form = document.querySelector(".form");
 
 
+    function emailValide(email){
+      var regex = /^[a-zA-Z0-9._-]+@etu\.univ-lyon1\.fr$/;
+
+      if(!regex.test(email.value)){
+        email.style.backgroundColor = "#ffe6eb";
+        return false;
+      } else {
+        email.style.backgroundColor = "#e7f5f2";
+        return true;
+      }
+
+    }
+
     email.addEventListener('input', function (event) {
-        email.style.backgroundColor = "red";
+        emailValide(email);
+    }, false);
+
+    form.addEventListener('submit', function (event) {
+        if(!emailValide(email)){
+
+          // On envoie pas le formulaire
+          event.preventDefault();
+        }
     }, false);
 
 };
